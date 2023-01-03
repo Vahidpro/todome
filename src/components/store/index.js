47a3a07947fd691dca31f2c87177b4ef51b53a22
Vahid Porki <ivahidp@gamil.com>
2023-01-03 16:19:04 +0330

    fixed redux store configuration

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskSlice from "./task-slice";
 
-const store = configureStore({ reducer: { task: taskSlice } });
+const store = configureStore({ reducer: { task: taskSlice.reducer } });
 
 export default store;
